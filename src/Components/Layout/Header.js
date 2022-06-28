c28e0d01feb2ab13db24bfbcb4fdee41e8b6bbd7
Sean Kennedy <snkennedy21@gmail.com>
2022-06-28 13:21:21 -0400

    launch project

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "./Header.module.css";
+
+const Header = function () {
+  return (
+    <React.Fragment>
+      <header>
+        <h1></h1>
+        <button></button>
+      </header>
+      <div>
+        <img />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
